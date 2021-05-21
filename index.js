61c80c12a50a3286f6944ac2f2375793f07a36a8
Adriana <am20dipi@alum.siena.edu>
2021-05-21 18:34:39 -0400

    commit tree c837364781d2f6eb2b00ca28395fed8f637f555b

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solution here
+
+
+function findMatching(drivers, Bob) {
+    return drivers.filter(match => 
+        match.toLowerCase() === Bob.toLowerCase()
+    )
+
+}
+
+function fuzzyMatch(drivers, B) {
+    return drivers.filter( match => 
+        match.toLowerCase().indexOf(B.toLowerCase()) === 0
+    )
+}
+
+function matchName(drivers, Adri) {
+    return drivers.filter( driver => driver.name == Adri)
+
+}
